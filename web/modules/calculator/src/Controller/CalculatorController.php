9a74f8838c54866f2f476196705c04c424a1c86e
noobj <jingfu99@gmail.com>
2023-06-30 01:27:23 -0700

    use node function for storing living cost data

--- web/modules/calculator/src/Controller/CalculatorController.php
+++ web/modules/calculator/src/Controller/CalculatorController.php
@@ -1,17 +1,44 @@
 <?php
 
-    namespace Drupal\calculator\Controller;
-    use Symfony\Component\HttpFoundation\Request;
-    use Drupal\Core\Controller\ControllerBase;
+namespace Drupal\calculator\Controller;
 
-    class CalculatorController extends ControllerBase  {
-      public function calculate(Request $request) {
-        var_dump($request->request);
-        $foo = $request->request->get('area');
+use Drupal\node\Entity\Node;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Controller\ControllerBase;
 
-        return [
-          '#theme' => 'my_template',
-          '#test_var' => $foo
-        ];
-      }
-    }
+class CalculatorController extends ControllerBase {
+
+	public function calculate(Request $request) {
+		$nids = \Drupal::entityQuery('node')->accessCheck(FALSE)->condition
+		('type', 'living_cost')
+			->execute();
+		$nodes = Node::loadMultiple($nids);
+
+		$list = [];
+		foreach ($nodes as $node) {
+			$category = $node->get('field_category')
+				->getString();
+			$list[$category][] = [
+				'title' => $node->getTitle(),
+				'price' => (int) $node->get('field_price')
+					->getString(),
+			];
+		}
+
+		return [
+			'#theme' => 'my_template',
+			'#nodes' => $list,
+		];
+	}
+
+	public function generateForm() {
+		$myform = \Drupal::formBuilder()
+			->getForm('Drupal\calculator\Form\CalculatorForm');
+
+		return [
+			'#theme' => 'my_form',
+			'#form' => $myform,
+		];
+	}
+
+}
