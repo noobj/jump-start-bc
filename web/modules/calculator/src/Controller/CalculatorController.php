dc675a7f5bf32bc85f5aefbe6571a4686653a0f4
noobj <jingfu99@gmail.com>
2023-07-03 18:11:19 -0700

    calculation feature

--- web/modules/calculator/src/Controller/CalculatorController.php
+++ web/modules/calculator/src/Controller/CalculatorController.php
@@ -8,26 +8,62 @@ use Drupal\Core\Controller\ControllerBase;
 
 class CalculatorController extends ControllerBase {
 
-	public function calculate(Request $request) {
+	public function calculate($area, $status, $kids, $car, $houseType) {
+		$adultsNumber = $status <= 0 ? 1 : 2;
+		$totalPeopleNumber = $adultsNumber + $kids;
 		$nids = \Drupal::entityQuery('node')->accessCheck(FALSE)->condition
 		('type', 'living_cost')
 			->execute();
+
+		$rentNids = \Drupal::entityQuery('node')->accessCheck(FALSE)->condition
+		('type', 'living_cost_rent')
+			->condition('field_area', $area)
+			->condition('field_house_type', $houseType)
+			->execute();
+
+		if ($car == false)
+			$carNids = \Drupal::entityQuery('node')->accessCheck(FALSE)->condition
+			('type', 'living_cost_transportation')
+				->condition('field_car', false)
+				->execute();
+		else
+			$carNids = \Drupal::entityQuery('node')->accessCheck(FALSE)->condition
+			('type', 'living_cost_transportation')
+				->execute();
+
+		$nids = array_merge($nids, $rentNids, $carNids);
 		$nodes = Node::loadMultiple($nids);
 
+		$total = 0;
 		$list = [];
 		foreach ($nodes as $node) {
+			$price = (int) $node->get('field_price')
+				->getString();
+
 			$category = $node->get('field_category')
 				->getString();
 			$list[$category][] = [
 				'title' => $node->getTitle(),
-				'price' => (int) $node->get('field_price')
-					->getString(),
+				'price' => $price,
 			];
+
+			if ($category != 'Rent')
+			$list[$category]['total'] += $price;
+
+			if ($category == 'Mobile')
+				$total += $adultsNumber * $price;
+			else if ($category == 'Food')
+				$total += $totalPeopleNumber * $price;
+			else
+				$total += $price;
 		}
 
 		return [
 			'#theme' => 'my_template',
 			'#nodes' => $list,
+			'#total' => $total,
+			'#adults' => $adultsNumber,
+			'#totalPeople' => $totalPeopleNumber
 		];
 	}
 
--- web/modules/calculator/src/Form/CalculatorForm.php
+++ web/modules/calculator/src/Form/CalculatorForm.php
@@ -5,109 +5,106 @@ namespace Drupal\calculator\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
-class CalculatorForm extends FormBase
-{
+class CalculatorForm extends FormBase {
 
-  public function getFormId()
-  {
-    return 'my_calculator';
-  }
+	public function getFormId() {
+		return 'my_calculator';
+	}
 
-  public function buildForm(array $form, FormStateInterface $form_state)
-  {
-    $form['status'] = array(
-      '#type' => 'select',
+	public function buildForm(array $form, FormStateInterface $form_state) {
+		$form['status'] = [
+			'#type' => 'select',
 
-      '#title' => t('Marital status'),
-      '#default_value' => 0,
-      '#options' => array(
-        0 => $this
-          ->t('Single'),
-        1 => $this
-          ->t('Married'),
-        2 => $this
-          ->t('Married With Kid(s)'),
-      ),
-      '#required' => TRUE,
-      '#attributes' => [
-        // Define a static id, so we can easier select it.
-        'id' => 'field_status_select',
-      ],
-    );
+			'#title' => t('Marital status'),
+			'#default_value' => 0,
+			'#options' => [
+				0 => $this
+					->t('Single'),
+				1 => $this
+					->t('Married'),
+				2 => $this
+					->t('Married With Kid(s)'),
+			],
+			'#required' => TRUE,
+			'#attributes' => [
+				// Define a static id, so we can easier select it.
+				'id' => 'field_status_select',
+			],
+		];
 
-    $form['kids'] = [
-      '#type' => 'number',
-      '#title' => t('How many kids do you have:'),
-      '#default_value' => 0,
-      '#min' => 0,
-      '#states' => [
-        // Show this textfield only if the radio 'other' is selected above.
-        'visible' => [
-          // Don't mistake :input for the type of field or for a css selector --
-          // it's a jQuery selector.
-          // You can always use :input or any other jQuery selector here, no matter
-          // whether your source is a select, radio or checkbox element.
-          ':input[id="field_status_select"]' => ['value' => '2'],
-        ],
-      ],
-    ];
+		$form['kids'] = [
+			'#type' => 'number',
+			'#title' => t('How many kids do you have:'),
+			'#default_value' => 0,
+			'#min' => 0,
+			'#states' => [
+				// Show this textfield only if the radio 'other' is selected above.
+				'visible' => [
+					// Don't mistake :input for the type of field or for a css selector --
+					// it's a jQuery selector.
+					// You can always use :input or any other jQuery selector here, no matter
+					// whether your source is a select, radio or checkbox element.
+					':input[id="field_status_select"]' => ['value' => '2'],
+				],
+			],
+		];
 
-    $form['area'] = array(
-      '#type' => 'radios',
-      '#title' => t('Area to live'),
-      '#default_value' => 'burnaby',
-      '#options' => [
-        'burnaby' => t('Burnaby'),
-        'downtown' => t('Downtown'),
-        'newwest' => t('New West'),
-      ],
-      '#required' => TRUE
-    );
+		$form['area'] = [
+			'#type' => 'radios',
+			'#title' => t('Area to live'),
+			'#default_value' => 'burnaby',
+			'#options' => [
+				'burnaby' => t('Burnaby'),
+				'downtown' => t('Downtown'),
+				'newwest' => t('New West'),
+			],
+			'#required' => TRUE,
+		];
 
-    $form['car'] = array(
-      '#type' => 'radios',
-      '#title' => t('Do you own a car?'),
-      '#default_value' => 'no',
-      '#options' => [
-        'yes' => t('Yes'),
-        'no' => t('No'),
-      ],
-      '#required' => TRUE
-    );
+		$form['houseType'] = [
+			'#type' => 'radios',
+			'#title' => t('Type of house'),
+			'#default_value' => 'studio',
+			'#options' => [
+				'share_house' => t('Share House'),
+				'studio' => t('Studio'),
+				'1_bedroom_apartment' => t('1 Bedroom'),
+				'2_bedroom_apartment' => t('2 Bedrooms')
+			],
+			'#required' => TRUE,
+		];
 
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Submit'),
-    ];
+		$form['car'] = [
+			'#type' => 'radios',
+			'#title' => t('Will you own a car?'),
+			'#default_value' => 0,
+			'#options' => [
+				true => t('Yes'),
+				false => t('No'),
+			],
+			'#required' => TRUE,
+		];
 
-    return $form;
-  }
+		$form['submit'] = [
+			'#type' => 'submit',
+			'#value' => $this->t('Submit'),
+		];
 
-//  public function validateForm(array &$form, FormStateInterface $form_state)
-//  {
-//    if (!is_numeric($form_state->getValue('first_digit'))) {
-//      $form_state->setErrorByName('first_digit', t('The first digit must be a valid numeric value'));
-//    }
-//
-//    if (!is_numeric($form_state->getValue('second_digit'))) {
-//      $form_state->setErrorByName('second_digit', t('The second digit must be a valid numeric value'));
-//    }
-//  }
+		return $form;
+	}
 
-  public function submitForm(array &$form, FormStateInterface $form_state)
-  {
-    $form_state->setRedirect('calculator_result', [],
-      [
-        'query' =>
-          [
-            'area' => $form_state->getValue('area'),
-            'status' => $form_state->getValue('status'),
-            'kids' => $form_state->getValue('kids'),
-            'car' => $form_state->getValue('car')
-          ]
-      ]
-    );
-  }
+	public function submitForm(array &$form, FormStateInterface $form_state) {
+		$form_state->setRedirect('calculator_result',
+			[
+				'area' => $form_state->getValue('area'),
+				'status' => $form_state->getValue('status'),
+				'kids' => $form_state->getValue('kids'),
+				'car' => $form_state->getValue('car'),
+				'houseType' => $form_state->getValue('houseType')
+			]
+		);
+	}
+
 }
 
 ?>
